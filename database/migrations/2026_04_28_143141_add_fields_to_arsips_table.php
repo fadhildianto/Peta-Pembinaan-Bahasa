c39160795f1b33a6300b56a223b768dd42df9e6b
fadhildianto@gmail.com <fadhildianto@gmail.com>
2026-04-28 21:34:46 +0700

    Migration Pertama

---
+++ database/migrations/2026_04_28_143141_add_fields_to_arsips_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('arsips', function (Blueprint $table) {
+            $table->enum('tipe_file', ['pdf', 'jpg', 'png', 'doc', 'docx'])->default('pdf')->after('nama_file');
+            $table->bigInteger('file_size')->nullable()->after('tipe_file');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('arsips', function (Blueprint $table) {
+            $table->dropColumn(['tipe_file', 'file_size']);
+        });
+    }
+};
